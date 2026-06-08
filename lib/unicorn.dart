8580d0d50e2a037264e6e198133907797d01a82d
CRamoysi <romain.civel@gmail.com>
2026-06-08 13:12:51 +0200

    Add Unicorn utility class with singleton pattern

--- lib/unicorn.dart
+++ lib/unicorn.dart
@@ -3,3 +3,24 @@
 
 export 'u_logger.dart';
 export 'u_collection_extensions.dart';
+
+/// Utilitaire Unicorn
+
+class U$ {
+  //singleton
+  static final U$ _instance = U$._internal();
+
+  factory U$() {
+    return _instance;
+  }
+
+  U$._internal();
+
+  // String icone licorn
+  static const String unicornIcon = "🦄";
+
+  // Flag mode debug
+  static bool get isDebug => kDebugMode;
+  static const bool forceDebug = false; // Toujours commit à false
+  static bool get canDebug => isDebug || forceDebug;
+}
